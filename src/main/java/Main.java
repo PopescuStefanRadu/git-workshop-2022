7d6b7626a3f257c8f93702aeb4a8a6f3b2168db4
Iftimie Adelin <adeadelin62@yahoo.com>
2022-07-18 16:06:47 +0300

    #2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // this is something that got added
+	System.out.println("Hello my name is Ade");
     }
 }
