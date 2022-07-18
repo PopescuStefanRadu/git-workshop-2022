259a0726a6cef11516e6c42ae4e38821d2148f27
Alex Lungu <alungu@hotmail.com>
2022-07-18 16:03:34 +0300

    Printed name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // this is something that got added
+        System.out.println("Alexandru Lungu was here.");
     }
 }
